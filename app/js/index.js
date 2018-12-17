06080b258360851c3db64fa0332aab3b01f53d94
Andy Vasilev <Andy.vasilev@gmail.com>
2018-12-17 19:34:29 +0300

    add jquery

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,26 @@
-$(document).ready(function() {
-    $(".content-edit").on("click", function() {
-      $(this).find('.click').addClass('edit');
-      $(this).find('.buttons').removeClass('hide');
+$(document).ready(function () {
+    function cleanUp(rollback = true) {
+        $('.buttons').remove();
+        $("[data-editable][contenteditable]").each(function () {
+            if ($(this).attr('data-old-value') && rollback) $(this).html($(this).attr('data-old-value'));
+            $(this).removeAttr('data-old-value').removeAttr('contenteditable');
+        })
+    }
+
+    $("[data-editable]").on("click", function () {
+
+        if ($(this).attr('contenteditable')) return;
+        cleanUp();
+        var buttons = $('body>.buttons-template').clone().removeClass('buttons-template').addClass('buttons');
+        $(this).attr('data-old-value', $(this).text()).attr('contenteditable', 'true').after(buttons);
+
+        $(buttons).on('click', '.save', function (e) {
+            cleanUp(false);
+        });
+
+        $(buttons).on('click', '.cancel', function (e) {
+            cleanUp();
+        });
     });
 
 });
