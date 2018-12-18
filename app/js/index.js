88c63e7ce1b06dd4748596075e604c054e143edc
Andy Vasilev <Andy.vasilev@gmail.com>
2018-12-18 16:15:31 +0300

    add skill

--- app/js/index.js
+++ app/js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         })
     }
 
-    $("[data-editable]").on("click", function () {
+    $('body').on("click", "[data-editable]", function () {
 
         if ($(this).attr('contenteditable')) return;
         cleanUp();
@@ -21,6 +21,46 @@ $(document).ready(function () {
         $(buttons).on('click', '.cancel', function (e) {
             cleanUp();
         });
+    });
+
+    $('body').on("click", ".skill-items>.skill-item>.cancel", function () {
+        $(this).parents('.skill-item').remove();
+    });
+    $('body').on("click", '.add-skill .save', function () {
+        var items = $('.skill-items');
+        var skillInput = $(this).parents('.add-skill').find('input.add');
+        var levelSelect = $(this).parents('.add-skill').find('select');
+        var skill = skillInput.val().trim();
+        var level = levelSelect.val().trim();
+        var found = false;
+        items.find('.skill-item').each(function (i) {
+            if ($(this).data('skill') && $(this).data('skill').toString().toLowerCase() === skill.toLowerCase()) {
+                found = true;
+                $(this).attr('data-level', level);
+            }
+        });
+        if (!found) {
+            var skillNode = $('.skill-item-template').clone()
+                .removeClass('skill-item-template')
+                .addClass('skill-item')
+                .attr('data-skill', skill)
+                .attr('data-level', level);
+            skillNode.find('span:first-child').html(skill);
+
+            $('.skill-items').append(skillNode);
+        }
+        skillInput.val('');
+
+    });
+
+    $('body').on('click', '[data-action=print]', function () {
+        if (window.print) {
+            window.print();
+        } else {
+            var WebBrowser = '<OBJECT ID="WebBrowser1" WIDTH=0 HEIGHT=0 CLASSID="CLSID:8856F961-340A-11D0-A96B-00C04FD705A2"></OBJECT>';
+            document.body.insertAdjacentHTML('beforeEnd', WebBrowser);
+            WebBrowser1.ExecWB(6, 2);//Use a 1 vs. a 2 for a prompting dialog box WebBrowser1.outerHTML = "";
+        }
     });
 
 });
